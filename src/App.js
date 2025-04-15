2026c0362f40ba8b0782c4a5a7e7df4b6861bd86
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-04-15 17:25:19 -0300

    usando porps passagem por parametros

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
        <Banner/>
-       <CampoTexto/>
+       <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+       <CampoTexto label="Cargo"  placeholder="Digite seu cargo"/>
+       <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
      
       
       </header>
