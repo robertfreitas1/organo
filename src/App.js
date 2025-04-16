cf3de22d5953e0c2c36d9d771b4ba73443dd47f2
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-04-16 15:02:56 -0300

    criando o formulário

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto/CampoTexto';
+import Formulario from './componentes/Formulario';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Banner/>
-       <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-       <CampoTexto label="Cargo"  placeholder="Digite seu cargo"/>
-       <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+      <Formulario/>
      
       
       </header>
