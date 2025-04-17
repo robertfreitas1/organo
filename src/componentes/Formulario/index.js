a3939f7af2ba827b38957803e73578f8cd39674e
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-04-17 17:54:43 -0300

    usando props e criando  a lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,17 @@
 import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () =>{
+    const times = [
+        'programação',
+        'Front=end',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
 return (
     <section className='formulario' >
         <form>
@@ -9,6 +19,7 @@ return (
         <CampoTexto label="Nome" placeholder="Digite seu nome"/>
        <CampoTexto label="Cargo"  placeholder="Digite seu cargo"/>
        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+       <ListaSuspensa label="Time" itens={times} />
        
         </form>
     </section>
