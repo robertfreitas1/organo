237afee71814ddb5fe18e5a04bd6d38e2d92d978
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-04-18 18:21:45 -0300

    inserindo texto e acessando através de porps e children

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../../Botao'
 import CampoTexto from '../CampoTexto/CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -20,6 +21,9 @@ return (
        <CampoTexto label="Cargo"  placeholder="Digite seu cargo"/>
        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
        <ListaSuspensa label="Time" itens={times} />
+       <Botao >
+        Criar card
+       </Botao>
        
         </form>
     </section>
