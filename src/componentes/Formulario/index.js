e243c0e44fc12a2d7cb5c9dfe4f527d9238a2c19
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-04-22 19:35:00 -0300

    escutanof eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -13,9 +13,14 @@ const Formulario = () =>{
         'Mobile',
         'Inovação e Gestão'
     ]
+const aoSalvar  = (evento) =>{
+    evento.preventDefault()
+    console.log('Form foi submetido')
+}
+
 return (
     <section className='formulario' >
-        <form>
+        <form onSubmit={ aoSalvar } >
             <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto label="Nome" placeholder="Digite seu nome"/>
        <CampoTexto label="Cargo"  placeholder="Digite seu cargo"/>
